d764a0e742c89f6d9b0fa5c7e99c10a6a5594584
Dawid Cieniecki <dawidowski7777@gmail.com>
2026-03-05 18:43:45 +0100

    poprawki formatowania kodu

--- Kalkulator Walut 2/Kalkulator Walut 2.cpp
+++ Kalkulator Walut 2/Kalkulator Walut 2.cpp
@@ -7,21 +7,22 @@ using namespace std;
 
 int main()
 {
-    map<string, float> ceny;
-    ceny["euro"] = 4.2;
-    ceny["dolar"] = 3.6;
-    ceny["frank"] = 4.6;
-    float kwota_poczatkowa;
+	map<string, float> ceny;
+	ceny["euro"] = 4.2;
+	ceny["dolar"] = 3.6;
+	ceny["frank"] = 4.6;
+	ceny["funt"] = 4.9;
+	float kwota_poczatkowa;
 
-        float kwota_koncowa;
-        string waluta;
-        cout << "Podaj ile pieniedzy chcesz wymienic" << endl;
-        cin >> kwota_poczatkowa;
-        cout << "Podaj walute" << endl;
-        cin >> waluta;
-        kwota_koncowa = kwota_poczatkowa / ceny[waluta];
+	float kwota_koncowa;
+	string waluta;
+	cout << "Podaj ile pieniedzy chcesz wymienic" << endl;
+	cin >> kwota_poczatkowa;
+	cout << "Podaj walute" << endl;
+	cin >> waluta;
+	kwota_koncowa = kwota_poczatkowa / ceny[waluta];
 
-        cout << "Kwota koncowa to: " << kwota_koncowa << endl;
+	cout << "Kwota koncowa to: " << kwota_koncowa << endl;
 
 
 }
